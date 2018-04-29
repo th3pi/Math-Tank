46e692d9813dcaf5cc9a0d2a07311045196a7bba
th3pi <38303217+th3pi@users.noreply.github.com>
2018-04-29 03:57:11 -0400

    Fixed bug that was causing app to crush on API lower than 22

--- app/src/main/java/nyc/tanjim/mathshark/MainMenu.java
+++ app/src/main/java/nyc/tanjim/mathshark/MainMenu.java
@@ -1,6 +1,7 @@
 package nyc.tanjim.mathshark;
 
 import android.graphics.drawable.AnimationDrawable;
+import android.os.Build;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,14 +13,18 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-        backgroundAnimation();
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.N) {
+            backgroundAnimation();
+        }
     }
 
     public void backgroundAnimation(){
-        ConstraintLayout constraintLayout = (findViewById(R.id.mainMenu));
-        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
-        animationDrawable.setEnterFadeDuration(5000);
-        animationDrawable.setExitFadeDuration(6000);
-        animationDrawable.start();
+            ConstraintLayout constraintLayout = (findViewById(R.id.mainMenu));
+            constraintLayout.setBackground(getResources().getDrawable(R.drawable.animation));
+            AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+            animationDrawable.setEnterFadeDuration(5000);
+            animationDrawable.setExitFadeDuration(6000);
+            animationDrawable.start();
+
     }
 }
