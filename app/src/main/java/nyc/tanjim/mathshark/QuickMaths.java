04fa6c0dc3d892d890f51a73d545fd2b989a43f8
th3pi <38303217+th3pi@users.noreply.github.com>
2018-04-29 17:34:25 -0400

    Finished setting up Quick Maths Layout

---
+++ app/src/main/java/nyc/tanjim/mathshark/QuickMaths.java
@@ -0,0 +1,17 @@
+package nyc.tanjim.mathshark;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class QuickMaths extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quick_maths);
+    }
+    public void choose(View view){
+        
+    }
+}
