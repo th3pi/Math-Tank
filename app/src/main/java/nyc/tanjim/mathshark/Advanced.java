f0d1185cbde1e34e3362c66b1cfb8a6e549d3f42
th3pi <38303217+th3pi@users.noreply.github.com>
2018-05-01 03:51:45 -0400

    Added tons of features. A pop up menu. Color changes according answer. Fixed logical errors. Updated scoreboard. New fonts.

--- app/src/main/java/nyc/tanjim/mathshark/Advanced.java
+++ app/src/main/java/nyc/tanjim/mathshark/Advanced.java
@@ -1,19 +1,29 @@
 package nyc.tanjim.mathshark;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Advanced extends AppCompatActivity {
+public class Advanced extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
     Button button0, button1, button2, button3;
     TextView questionText, scoreView;
+    ImageButton menuButton;
     int locationOfCorrectAnswer, score = 0, numberOfQuestions = 0;
     ArrayList<Integer> answers = new ArrayList<Integer>();
+    ConstraintLayout bg;
 
 
     @Override
@@ -26,17 +36,73 @@ public class Advanced extends AppCompatActivity {
         button3 = findViewById(R.id.button3);
         questionText = findViewById(R.id.quiestionsView);
         scoreView = findViewById(R.id.scoreView);
+        bg = findViewById(R.id.bg);
+        menuButton = findViewById(R.id.menuButton);
 
+        //Creates and inflates the top left pop up menu
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(Advanced.this, v);
+                popup.setOnMenuItemClickListener(Advanced.this);
+                popup.inflate(R.menu.popup);
+                popup.show();
+            }
+        });
+
+        //Generate the starting question
         generateQuestion();
     }
+
+    //Adds functionality to the pop up menu
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.home:
+                Intent intent = new Intent(getApplicationContext(), MainMenu.class);
+                startActivity(intent);
+                return true;
+            case R.id.settings:
+                return true;
+            case R.id.reset:
+                reset();
+                return true;
+            case R.id.exit:
+                moveTaskToBack(true);
+                android.os.Process.killProcess(android.os.Process.myPid());
+                System.exit(1);
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    //Answer button's functionality
     public void choose(View view){
+
+        //gets the difference between number of questions answered and score
+        double difference;
+        difference = numberOfQuestions - score;
+
+        //if statement connects the tapped answer to the correct answer
         if(view.getTag().toString().equals(Integer.toString(locationOfCorrectAnswer))) {
-            generateQuestion();
             score++;
             numberOfQuestions++;
+            generateQuestion();
+            bg.setBackground(ContextCompat.getDrawable(getApplicationContext(),R.drawable.correct_gradient_1));
+            scoreView.setTextColor(Color.WHITE);
+            questionText.setTextColor(Color.WHITE);
+            menuButton.setImageResource(R.drawable.ic_menu_white);
         }else {
             numberOfQuestions++;
+            generateQuestion();
+            bg.setBackground(ContextCompat.getDrawable(getApplicationContext(),R.drawable.wrong_gradient_1));
+            scoreView.setTextColor(Color.WHITE);
+            questionText.setTextColor(Color.WHITE);
+            menuButton.setImageResource(R.drawable.ic_menu_white);
         }
+        scoreView.setText(Integer.toString(score) + "/" + Integer.toString(numberOfQuestions));
+
 
         answers.clear();
     }
@@ -51,7 +117,10 @@ public class Advanced extends AppCompatActivity {
             multiplyQuestions();
         }else if(a == 3){
             divisionQuestion();
+        }else{
+            sumQuestions();
         }
+        answers.clear();
     }
 
     public void sumQuestions(){
@@ -59,7 +128,7 @@ public class Advanced extends AppCompatActivity {
         int a = rd.nextInt(20)+1;
         int b = rd.nextInt(20)+1;
         int incorrectAnswer;
-        int locationOfCorrectAnswer = rd.nextInt(4);
+        locationOfCorrectAnswer = rd.nextInt(4);
 
         //Updates QuestionBoard
         questionText.setText(Integer.toString(a) + "+" + Integer.toString(b));
@@ -119,13 +188,13 @@ public class Advanced extends AppCompatActivity {
     }
     public void divisionQuestion(){
         Random rd = new Random();
-        int a = rd.nextInt(10)+1;
-        int b = rd.nextInt(10)+a;
+        int a = rd.nextInt(20)+1;
+        int b = rd.nextInt(60)+a;
         int incorrectAnswer;
-        int locationOfCorrectAnswer = rd.nextInt(4);
-        while(b % a != 0){
+        locationOfCorrectAnswer = rd.nextInt(4);
+        while(b % a != 0 || b == a){
             a = rd.nextInt(10)+1;
-            b = rd.nextInt(10)+a;
+            b = rd.nextInt(60)+a;
         }
         //Updates QuestionBoard
         questionText.setText(Integer.toString(b) + "/" + Integer.toString(a));
@@ -156,7 +225,7 @@ public class Advanced extends AppCompatActivity {
         int a = rd.nextInt(50)+1;
         int b = rd.nextInt(50)+a;
         int incorrectAnswer;
-        int locationOfCorrectAnswer = rd.nextInt(4);
+        locationOfCorrectAnswer = rd.nextInt(4);
 
         //Updates QuestionBoard
         questionText.setText(Integer.toString(b) + "-" + Integer.toString(a));
@@ -182,4 +251,19 @@ public class Advanced extends AppCompatActivity {
         button2.setText(Integer.toString(answers.get(2)));
         button3.setText(Integer.toString(answers.get(3)));
     }
+
+    //Resets current session
+    public void reset(){
+        score = 0;
+        numberOfQuestions = 0;
+        scoreView.setText("0/0");
+        generateQuestion();
+        bg.setBackgroundColor(0xffffff);
+        scoreView.setTextColor(Color.GRAY);
+        questionText.setTextColor(Color.GRAY);
+        menuButton.setImageResource(R.drawable.ic_menu);
+
+    }
+
+
 }
--- app/src/main/java/nyc/tanjim/mathshark/MainMenu.java
+++ app/src/main/java/nyc/tanjim/mathshark/MainMenu.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Build;
 import android.support.constraint.ConstraintLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -27,7 +28,7 @@ public class MainMenu extends AppCompatActivity {
 
     public void backgroundAnimation(){
             ConstraintLayout constraintLayout = (findViewById(R.id.mainMenu));
-            constraintLayout.setBackground(getResources().getDrawable(R.drawable.animation));
+            constraintLayout.setBackground(ContextCompat.getDrawable(getApplicationContext(),R.drawable.animation));
             AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
             animationDrawable.setEnterFadeDuration(5000);
             animationDrawable.setExitFadeDuration(6000);
