3c81f5e96325cd104e56da8a8844024883f017f0
th3pi <38303217+th3pi@users.noreply.github.com>
2018-05-06 16:08:04 -0400

    Added new animation to main menu buttons

--- app/src/main/java/nyc/tanjim/mathshark/MainMenu.java
+++ app/src/main/java/nyc/tanjim/mathshark/MainMenu.java
@@ -8,13 +8,27 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
-
+    Button quickMathButton, timeTrialsButton, advancedMathButton;
+    Animation fromLeftQuickMath, fromLeftTimeTrials, fromLeftAdvanced;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        quickMathButton = findViewById(R.id.quickMathsButton);
+        timeTrialsButton = findViewById(R.id.timeTrialsButton);
+        advancedMathButton = findViewById(R.id.advancedMathButton);
+
+        fromLeftQuickMath = AnimationUtils.loadAnimation(this,R.anim.from_left_quick_math);
+        quickMathButton.setAnimation(fromLeftQuickMath);
+        fromLeftTimeTrials = AnimationUtils.loadAnimation(this,R.anim.from_left_time_trials);
+        timeTrialsButton.setAnimation(fromLeftTimeTrials);
+        fromLeftAdvanced = AnimationUtils.loadAnimation(this, R.anim.from_left_advanced);
+        advancedMathButton.setAnimation(fromLeftAdvanced);
 
         if(Build.VERSION.SDK_INT > Build.VERSION_CODES.N) {
             backgroundAnimation();
