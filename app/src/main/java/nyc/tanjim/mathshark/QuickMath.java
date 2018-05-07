8f91a5f24afcc4bc827f8d88591618b970fed4e6
th3pi <38303217+th3pi@users.noreply.github.com>
2018-05-07 07:27:08 -0400

    Tons of new features. - Vibration on wrong answer - New custom pop up - Custom winning message depending on score

--- app/src/main/java/nyc/tanjim/mathshark/QuickMath.java
+++ app/src/main/java/nyc/tanjim/mathshark/QuickMath.java
@@ -1,5 +1,6 @@
 package nyc.tanjim.mathshark;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -7,6 +8,7 @@ import android.content.SharedPreferences;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Build;
 import android.os.CountDownTimer;
+import android.os.Vibrator;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
@@ -19,10 +21,12 @@ import android.widget.TextView;
 import java.util.Random;
 
 public class QuickMath extends AppCompatActivity {
-    TextView timerText, quickMathQuestion, quickMathScore, scoreSpread, lastScoreText;
-    Button correctButton, wrongButton;
+    TextView timerText, quickMathQuestion, quickMathScore, scoreSpread, lastScoreText, winningMessage;
+    TextView scoreMessage, iqMessage;
+    Button correctButton, wrongButton, playAgainButton, quitButton;
     int correctAnswer, score, wrongOrCorrect, numberOfQuestions;
-    SharedPreferences lastScore;
+    Dialog scorePopUp;
+    Vibrator vibrator;
 
 
     @Override
@@ -35,11 +39,39 @@ public class QuickMath extends AppCompatActivity {
         correctButton = findViewById(R.id.correctButton);
         wrongButton = findViewById(R.id.wrongButton);
         scoreSpread = findViewById(R.id.scorespread);
-        lastScore = this.getSharedPreferences("nyc.tanjim.mathshark", Context.MODE_PRIVATE);
+        scorePopUp = new Dialog(this);
+        scorePopUp.setContentView(R.layout.score_popup);
+        playAgainButton = scorePopUp.findViewById(R.id.playAgainButton);
+        quitButton = scorePopUp.findViewById(R.id.quitButton);
+        winningMessage = scorePopUp.findViewById(R.id.winningMessage);
+        scoreMessage = scorePopUp.findViewById(R.id.scoreMessage);
+        iqMessage = scorePopUp.findViewById(R.id.iqMessage);
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         generateQuestion();
         backgroundAnimation();
         timer();
     }
+    //TODO create an algorithm to calibrate IQ
+    public void showPopUp(){
+        if(numberOfQuestions - score == 0 && numberOfQuestions > 10){
+            winningMessage.setText("Hello there genius!");
+        }else if(numberOfQuestions - score > 0 && numberOfQuestions - score < 5 && numberOfQuestions > 10) {
+            winningMessage.setText("Unbelievable!");
+        }else {
+            winningMessage.setText("Need more practice!");
+        }
+        scoreMessage.setText(getString(R.string.score_pop_score, score, numberOfQuestions));
+        scorePopUp.setCanceledOnTouchOutside(false);
+        scorePopUp.show();
+    }
+
+    public void playAgain(View view){
+        recreate();
+    }
+    public  void quit(View view){
+        finish();
+    }
+
     public void backgroundAnimation(){
         ConstraintLayout constraintLayout = (findViewById(R.id.quickMathBg));
         constraintLayout.setBackground(ContextCompat.getDrawable(getApplicationContext(),R.drawable.animation));
@@ -55,6 +87,7 @@ public class QuickMath extends AppCompatActivity {
             quickMathScore.setText("SCORE: " + Integer.toString(score));
         }else{
             generateQuestion();
+            vibrator.vibrate(500);
         }
 
     }
@@ -143,7 +176,7 @@ public class QuickMath extends AppCompatActivity {
 
     //Timer that keeps track of time
     public void timer(){
-        new CountDownTimer(2000,1000) {
+        new CountDownTimer(30000,1000) {
 
             @Override
             public void onTick(long millisUntilFinished) {
@@ -157,7 +190,8 @@ public class QuickMath extends AppCompatActivity {
 
             @Override
             public void onFinish() {
-                message();
+               showPopUp();
+
             }
         }.start();
     }
@@ -170,6 +204,8 @@ public class QuickMath extends AppCompatActivity {
         generateQuestion();
     }
     //This method creates an alert box once the timer hits zero
+
+    //this is redundant
     public void message(){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage("Your answered " + Integer.toString(score) + " correctly out of" +
@@ -192,5 +228,6 @@ public class QuickMath extends AppCompatActivity {
             dialog.show();
         }
     }
+
 
 }
