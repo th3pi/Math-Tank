2ebc4a17ba23ea95228d4815bd1aeea1e66aa37a
th3pi <38303217+th3pi@users.noreply.github.com>
2018-05-13 22:07:20 -0400

    Made some ui updates to main menu

---
+++ app/src/main/java/nyc/tanjim/mathshark/SplashScreen.java
@@ -0,0 +1,30 @@
+package nyc.tanjim.mathshark;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        final Intent i = new Intent(SplashScreen.this,MainMenu.class);
+        Thread welcomeThread = new Thread() {
+            @Override
+            public void run(){
+                try{
+                    super.run();
+                    sleep(2000);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        welcomeThread.start();
+    }
+}
