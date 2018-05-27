471ec05f506140ad456adc73b054dd426894f238
th3pi <38303217+th3pi@users.noreply.github.com>
2018-05-27 01:17:55 -0400

    Added proper splash screen

--- app/src/main/java/nyc/tanjim/mathtank/MainMenu.java
+++ app/src/main/java/nyc/tanjim/mathtank/MainMenu.java
@@ -67,6 +67,7 @@ public class MainMenu extends AppCompatActivity implements SharedPreferences.OnS
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
         quickMathButton = findViewById(R.id.quickMathsButton);
